a831ca7aade3356e575a4c044c9152c6f9e080ae
fc410 <54721672+fc410@users.noreply.github.com>
2020-02-26 18:55:53 -0800

    created the venv and install all the libraries needed

---
+++ stock-script.py
@@ -0,0 +1,11 @@
+#pip install beautifulsoup4
+#pip install XlsxWriter
+#pip install selenium
+#pip install phantomjs
+#pip install matplotlib
+#pip install mpl_finance
+#pip install pandas
+#pip install pandas-reader
+#pip install yfinance
+#pip install fix_yahoo_finance
+
