c2c87bd1d5d51f0372f902090289ff2778d46e55
fc410 <54721672+fc410@users.noreply.github.com>
2020-02-26 19:00:33 -0800

    s

--- stock-script.py
+++ stock-script.py
@@ -5,7 +5,15 @@
 #pip install matplotlib
 #pip install mpl_finance
 #pip install pandas
-#pip install pandas-reader
+#pip install pandas-datareader
 #pip install yfinance
 #pip install fix_yahoo_finance
 
+import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader as pdr
+import pandas_datareader.data as web
+import pickle
+import requests
