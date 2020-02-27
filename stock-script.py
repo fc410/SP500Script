947ebdee973196363c332353fe80dfb4781450ab
fc410 <54721672+fc410@users.noreply.github.com>
2020-02-26 19:18:40 -0800

    a function that will get the s&p 500 companies

--- stock-script.py
+++ stock-script.py
@@ -13,7 +13,32 @@ import bs4 as bs
 import datetime as dt
 import os
 import pandas as pd
-import pandas_datareader as pdr
-import pandas_datareader.data as web
+from pandas_datareader import data as pdr
 import pickle
 import requests
+import fix_yahoo_finance as yf
+
+
+
+#this function will scrape the s&p 500 companies
+#from wikipedia and will save it into an array 
+#called tickers
+def save_sp500_tickers():
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    
+    tickers = []
+
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text.replace('.', '-')
+        ticker = ticker[:-1]
+        tickers.append(ticker)
+
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+
+    print(tickers)
+    return tickers
+
+#save_sp500_tickers()
